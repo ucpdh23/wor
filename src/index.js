0400bc5fea5bf48d55f5aaaaa4cb968e034581c8
xan.xan@gmail.com <xan.xan@gmail.com>
2021-06-17 20:23:01 +0200

    adding index

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+*/
+// Middlewares
 
+// Routes
+app.use(express.static(__dirname + '/public'));
+
+// Server is listening
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
