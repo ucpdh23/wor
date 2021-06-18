2a3bcb1183be765b422223854b83700d466ee912
xan.xan@gmail.com <xan.xan@gmail.com>
2021-06-18 23:15:46 +0200

    websocket

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+const { Server } = require('ws');
+
+
 /*app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -17,6 +20,19 @@ app.use(express.static(__dirname + '/public'));
 
 
 // Server is listening
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+const wss = new Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+});
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
