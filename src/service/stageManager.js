01848a35d57ddecb9f04603c31c82bdb7da37ba4
xan <xan.xan@gmail.com>
2023-06-04 09:46:32 +0200

    updating stage

--- src/service/stageManager.js
+++ src/service/stageManager.js
@@ -129,15 +129,15 @@ function populateTeams(stage) {
 
 function populateProfile(stage) {
     var etapa = [
-      0, 2, 4, 7, 2,
-      -3, -5, -5, -3, 0,
+      0, 2, 4, 6, 2,
+      -3, -5, -4, -3, 0,
       0, 0, 0, 4, 5,
       7, 8, 6, 7, 8,
-      9, 10, -3, -6, -6,
+      8, 7, -3, -6, -6,
       -6, -7, -2, -5, -7,
       -9, -2, 0, 0, 0,
       0, 0, 4, 0, 6,
-      7, 12, 15, 3];
+      7, 11, 10, 3];
 
     var clasificacion = new Clasificacion();
     var profile = new Profile(clasificacion, etapa, 1000);
